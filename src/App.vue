056f88dfba66b52d5b2803f230df5e81f8e3a42d
truejojo <johannesveh@yahoo.de>
2023-08-18 11:33:15 +0200

    ready to start

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,121 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      cards: [
+        {
+          status: 0,
+          title: "Neue Aufgaben",
+          bgColor: "danger",
+          newTask: true,
+        },
+        {
+          status: 1,
+          title: "In Bearbeitung",
+          bgColor: "primary",
+          newTask: false,
+        },
+        {
+          status: 2,
+          title: "Erledigt",
+          bgColor: "success",
+          newTask: false,
+        },
+      ],
+      tasks: [
+        {
+          id: 1,
+          content: "Dashboard überarbeiten.",
+          status: 0,
+        },
+        {
+          id: 2,
+          content: "Anwendung auf Vue.js umstellen.",
+          status: 0,
+        },
+        {
+          id: 3,
+          content: "Lorem ipsum dolor sit amet.",
+          status: 1,
+        },
+        {
+          id: 4,
+          content: "Lorem ipsum dolor.",
+          status: 2,
+        },
+        {
+          id: 5,
+          content: "Lorem ipsum sit amet.",
+          status: 2,
+        },
+        {
+          id: 6,
+          content: "Lorem, ipsum.",
+          status: 2,
+        },
+        {
+          id: 7,
+          content: "Kein Internet...",
+          status: 1,
+        },
+      ],
+    };
+  },
+  computed: {
+    newTasks() {
+      return this.tasks.filter((task) => task.status === 0);
+    },
+  },
+};
 </script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-4">
+        <div class="card">
+          <div class="card-header text-center bg-secondary text-white">
+            <h4>Neue Aufgaben</h4>
+          </div>
+          <div class="card-body">
+            <div
+              class="alert alert-secondary"
+              v-for="task in newTasks"
+              :key="task.id"
+            >
+              {{ task.content }}
+            </div>
+          </div>
+          <div class="card-footer">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Neue Aufgabe"
+            />
+            <div class="d-grid my-2">
+              <button class="btn btn-secondary">Eintragen</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card">
+          <div class="card-header text-center bg-primary text-white">
+            <h4>In Bearbeitung</h4>
+          </div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card">
+          <div class="card-header text-center bg-success text-white">
+            <h4>Erledigt</h4>
+          </div>
+          <div class="card-body"></div>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.css";
+
+createApp(App).mount("#app");
+
+import "bootstrap/dist/js/bootstrap.js";
